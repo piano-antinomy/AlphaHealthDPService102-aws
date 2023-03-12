68c619db12eb0b303330bcb369048ab7b0a6ae1e
Yingjian Ding <james.yingjian.ding@gmail.com>
2023-03-12 12:31:51 -0700

    fix code style

--- src/main/java/com/alpha/health/dp/core/lambda/util/Duration.java
+++ src/main/java/com/alpha/health/dp/core/lambda/util/Duration.java
@@ -1,7 +1,12 @@
 package com.alpha.health.dp.core.lambda.util;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 
 @Getter
 @Builder
